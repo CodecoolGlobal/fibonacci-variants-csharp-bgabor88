a415f0470b40537b81a88fdf775056234fe97ce9
appuser <appuser@journey2-7789ff4cf5-q8k7p>
2022-02-07 08:08:49 +0000

    starter project

---
+++ src/Codecool.FibonacciVariants/FibonacciVariants.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.FibonacciVariants
+{
+    public static class FibonacciVariants
+    {
+        public static int AdditionsCounter { get; private set; }
+
+        public static void ResetCounter()
+        {
+            AdditionsCounter = 0;
+        }
+
+        public static int Iterative(int n)
+        {
+            return 0;
+        }
+
+        public static int NaiveRecursive(int n)
+        {
+            return 0;
+        }
+
+        public static int RecursiveWithMemoization(int n)
+        {
+            return 0;
+        }
+
+        public static int TailRecursive(int n)
+        {
+            return 0;
+        }
+
+        private static int TailRecursiveHelper(int n, int a, int b)
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/Codecool.FibonacciVariants/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static Codecool.FibonacciVariants.FibonacciVariants;
+
+namespace Codecool.FibonacciVariants
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int k = 30;
+
+            ResetCounter();
+            PrintFibonacci("iteration", k, Iterative(k), AdditionsCounter);
+            
+            ResetCounter();
+            PrintFibonacci("naive recursion", k, NaiveRecursive(k), AdditionsCounter);
+            
+            ResetCounter();
+            PrintFibonacci("recursion with memoization", k, RecursiveWithMemoization(k), AdditionsCounter);
+            
+            ResetCounter();
+            PrintFibonacci("tail recursion", k, TailRecursive(k), AdditionsCounter);
+        }
+
+        private static void PrintFibonacci(string methodName, int k, int fib, int additions)
+        {
+            Console.WriteLine($"Using {methodName}: Fib({k}) = {fib}, number of additions: {additions}");
+        }
+    }
+}
